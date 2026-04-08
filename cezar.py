ad75f07265028d6eecdb47ec7449154c5d3798d0
tomaszkrawczyk1 <tomek7839.krawczyk@gmail.com>
2026-04-08 09:59:23 +0200

    Poprawa implementacji szyfru Cezara

--- cezar.py
+++ cezar.py
@@ -1,9 +1,16 @@
-def cezar(tekst: str, klucz: int):
+def szyfruj_cezara(tekst: str, klucz: int) -> str:
     wynik = ""
+
     for litera in tekst:
+        if litera.isalpha():
             kod_bazowy = 65 if litera.isupper() else 97
             nowy_kod = (ord(litera) - kod_bazowy + klucz) % 26 + kod_bazowy
             wynik += chr(nowy_kod)
+        else:
+            wynik += litera
+
     return wynik
 
-print(cezar("haslo", 3))
+
+if __name__ == "__main__":
+    print(szyfruj_cezara("HASLO", 3))
